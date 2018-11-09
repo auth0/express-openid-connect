ca81f326dffbab399c9d0c25432f14db2991ec88
José F. Romaniello <jfromaniello@gmail.com>
2018-11-09 10:56:33 -0300

    minor

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const _ = require('lodash');
 
 Issuer.defaultHttpOptions = { timeout: 4000 };
 
+/**
+* Returns a middleware that verifies the existence of req.session.user.
+* If "user" is not in the session it will redirect to /login,
+* otherwise continue to the next middleware in the stack.
+*/
 module.exports.protect = function() {
   return function(req, res, next) {
     if (req.session.user) { return next(); }
