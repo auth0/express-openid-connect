38df12335099db95a811d962e416c891bfaaa5dc
Josh Cunningham <josh@joshcanhelp.com>
2020-01-03 15:07:56 -0800

    Add missing TS definitions

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
 // Type definitions for express-openid-connect
 
 import { AuthorizationParameters, TokenSet, UserinfoResponse } from 'openid-client';
-import { Request, RequestHandler } from 'express';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 interface ConfigParams {
+    appSessionCookie?: SessionCookieConfigParams;
+    appSessionDuration?: number;
+    appSessionName?: string;
+    appSessionSecret: boolean | string | string[];
     auth0Logout?: boolean;
     authorizationParams?: AuthorizationParameters
     baseURL?: string;
@@ -11,15 +15,27 @@ interface ConfigParams {
     clientSecret?: string;
     clockTolerance?: number;
     errorOnRequiredAuth?: boolean;
-    getUser?: (tokenSet: TokenSet) => undefined | UserinfoResponse;
+    getUser?: (req: Request, config: ConfigParams) => undefined | UserinfoResponse;
+    handleCallback?: RequestHandler;
+    httpOptions?: object;
+    identityClaimFilter?: string[];
     idpLogout?: boolean;
     idTokenAlg?: string;
     issuerBaseURL?: string;
+    legacySameSiteCookie?: boolean;
     loginPath?: string;
     logoutPath?: string;
     redirectUriPath?: string;
     required?: boolean | ((request: Request) => boolean);
     routes?: boolean;
+}
+
+interface SessionCookieConfigParams {
+    domain?: string;
+    httpOnly?: boolean;
+    path?: string;
+    sameSite?: string;
+    secure?: boolean;
 }
 
 export function auth(params?: ConfigParams): RequestHandler;
