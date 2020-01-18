b4f6c24ad78f2269d70350c6b18c4ff7e8827c99
Josh Cunningham <josh@joshcanhelp.com>
2020-01-17 16:42:20 -0800

    Add tests for cookies set; avoid config mutation

--- test/appSession.tests.js
+++ test/appSession.tests.js
@@ -5,21 +5,20 @@ const sessionEncryption = require('./fixture/sessionEncryption');
 const defaultConfig = {
   name: 'identity',
   secret: '__test_secret__',
-  duration: 1234567890,
+  duration: 3155760000, // 100 years
   cookieOptions: {}
 };
 
 const req = {
   get: (key) => key
 };
-const res = {};
 const next = () => true;
 
 describe('appSession', function() {
 
   describe('no session cookies, no session property', () => {
     const appSessionMw = appSession(defaultConfig);
-    const result = appSessionMw(req, res, next);
+    const result = appSessionMw(req, {}, next);
 
     it('should call next', function() {
       assert.ok(result);
@@ -33,7 +32,7 @@ describe('appSession', function() {
   describe('no session cookies, existing session property', () => {
     const appSessionMw = appSession(defaultConfig);
     const thisReq = Object.assign({}, req, {identity: {sub: '__test_existing_sub__'}});
-    const result = appSessionMw(thisReq, res, next);
+    const result = appSessionMw(thisReq, {}, next);
 
     it('should call next', function() {
       assert.ok(result);
@@ -49,7 +48,7 @@ describe('appSession', function() {
     const thisReq = {get: () => 'identity=__invalid_identity__'};
 
     it('should error with malformed identity', function() {
-      assert.throws(() => appSessionMw(thisReq, res, next), Error, 'JWE malformed or invalid serialization');
+      assert.throws(() => appSessionMw(thisReq, {}, next), Error, 'JWE malformed or invalid serialization');
     });
   });
 
@@ -58,9 +57,83 @@ describe('appSession', function() {
     const thisReq = {get: () => 'identity=' + sessionEncryption.encrypted};
 
     it('should set the identity on req', function() {
-      const result = appSessionMw(thisReq, res, next);
+      const result = appSessionMw(thisReq, {}, next);
       assert.ok(result);
       assert.equal(thisReq.identity.sub, '__test_sub__');
+
+    });
+  });
+
+  describe('sessioncookie options', () => {
+    let cookieArgs;
+    const thisRes = {
+      cookie: function cookie() {cookieArgs = JSON.parse(JSON.stringify(arguments)); },
+      writeHead: () => null,
+      setHeader: () => null
+    };
+
+    beforeEach(function() {
+      cookieArgs = {};
+    });
+
+    it('should set the correct cookie by default', function() {
+      const thisReq = {get: () => 'identity=' + sessionEncryption.encrypted};
+      const appSessionMw = appSession(defaultConfig);
+      const result = appSessionMw(thisReq, thisRes, next);
+      thisRes.writeHead();
+
+      assert.ok(result);
+      assert.equal(cookieArgs['0'], 'identity');
+      assert.isNotEmpty(cookieArgs['1']);
+      assert.isObject(cookieArgs['2']);
+      assert.hasAllKeys(cookieArgs['2'], ['expires']);
+
+      const expDate = new Date(cookieArgs['2'].expires);
+      assert.equal( (expDate.getFullYear() - (new Date()).getFullYear()), 100);
+    });
+
+    it('should set the correct custom cookie name', function() {
+      const thisReq = {get: () => 'customName=' + sessionEncryption.encrypted};
+      const customConfig = Object.assign({}, defaultConfig, {name: 'customName'});
+      const appSessionMw = appSession(customConfig);
+      const result = appSessionMw(thisReq, thisRes, next);
+      thisRes.writeHead();
+
+      assert.ok(result);
+      assert.equal(cookieArgs['0'], 'customName');
+    });
+
+    it('should set an ephemeral cookie', function() {
+      const thisReq = {get: () => 'identity=' + sessionEncryption.encrypted};
+      const customConfig = Object.assign({}, defaultConfig, {cookieOptions: {ephemeral: true}});
+      const appSessionMw = appSession(customConfig);
+      const result = appSessionMw(thisReq, thisRes, next);
+      thisRes.writeHead();
+
+      assert.ok(result);
+      assert.equal(cookieArgs['2'].expires, 0);
+    });
+
+    it('should pass custom cookie options', function() {
+      const thisReq = {get: () => 'identity=' + sessionEncryption.encrypted};
+      const cookieOptConfig = {cookieOptions: {
+        domain: '__test_domain__',
+        path: '__test_path__',
+        secure: true,
+        httpOnly: false,
+        sameSite: '__test_samesite__',
+      }};
+      const customConfig = Object.assign({}, defaultConfig, cookieOptConfig);
+      const appSessionMw = appSession(customConfig);
+      const result = appSessionMw(thisReq, thisRes, next);
+      thisRes.writeHead();
+
+      assert.ok(result);
+      assert.equal(cookieArgs['2'].domain, '__test_domain__');
+      assert.equal(cookieArgs['2'].path, '__test_path__');
+      assert.equal(cookieArgs['2'].secure, true);
+      assert.equal(cookieArgs['2'].httpOnly, false);
+      assert.equal(cookieArgs['2'].sameSite, '__test_samesite__');
     });
   });
 });
