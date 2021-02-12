c542a5e5bc40bae4f217a992281a57a8bd083ad6
David <david.patrick@auth0.com>
2021-02-12 00:17:47 -0800

    Add custom store tests

---
+++ test/appSession.customStore.tests.js
@@ -0,0 +1,447 @@
+const { promisify } = require('util');
+const assert = require('chai').assert;
+const crypto = require('crypto');
+const request = require('request-promise-native').defaults({
+  simple: false,
+  resolveWithFullResponse: true,
+});
+const sinon = require('sinon');
+
+const appSession = require('../lib/appSession');
+const { encrypted } = require('./fixture/sessionEncryption');
+const { makeIdToken } = require('./fixture/cert');
+const { get: getConfig } = require('../lib/config');
+const { create: createServer } = require('./fixture/server');
+const redis = require('redis-mock');
+const RedisStore = require('connect-redis')({ Store: class Store {} });
+
+const defaultConfig = {
+  clientID: '__test_client_id__',
+  clientSecret: '__test_client_secret__',
+  issuerBaseURL: 'https://op.example.com',
+  baseURL: 'http://example.org',
+  secret: '__test_secret__',
+  errorOnRequiredAuth: true,
+};
+
+const login = async (claims) => {
+  const jar = request.jar();
+  await request.post('/session', {
+    baseUrl,
+    jar,
+    json: {
+      id_token: makeIdToken(claims),
+    },
+  });
+  return jar;
+};
+
+const baseUrl = 'http://localhost:3000';
+
+const HR_MS = 60 * 60 * 1000;
+
+describe.only('appSession', () => {
+  let server;
+  let redisClient;
+  let store;
+
+  const setup = async (config) => {
+    redisClient = redis.createClient();
+    store = new RedisStore({ client: redisClient, prefix: '' });
+    store.asyncSet = promisify(store.set).bind(store);
+    store.asyncGet = promisify(store.set).bind(store);
+
+    const conf = getConfig({
+      ...defaultConfig,
+      sessionStore: store,
+      ...config,
+    });
+
+    server = await createServer(appSession(conf));  
+  };
+
+  afterEach(async () => {
+    if (server) {
+      await new Promise((resolve) => server.close(resolve));
+    }
+    if (redisClient) {
+      await new Promise((resolve) => redisClient.quit(resolve));
+    }
+  });
+
+  it('should not create a session when there are no cookies', async () => {
+    await setup();
+    const res = await request.get('/session', { baseUrl, json: true });
+    assert.isEmpty(res.body);
+  });
+
+  it('should not error for non existent sessions', async () => {
+    await setup();
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      headers: {
+        cookie: 'appSession=__invalid_identity__',
+      },
+    });
+    assert.equal(res.statusCode, 200);
+    assert.isEmpty(res.body);
+  });
+
+  it('should get an existing session', async () => {
+    const sessionData = JSON.stringify({ data: { sub: '__test_sub__' } });
+    await setup();
+    await new Promise((resolve) =>
+      store.set('foo', sessionData, () =>
+        resolve()
+      )
+    );
+    await new Promise((resolve) =>
+      store.get('foo', (err, x) => {
+        assert.equal(x, sessionData);
+        resolve();
+      })
+    );
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      headers: {
+        cookie: `appSession=foo`,
+      },
+    });
+    assert.equal(res.statusCode, 200);
+  });
+
+  it('should chunk and accept chunked cookies over 4kb', async () => {
+    await setup();
+    const jar = request.jar();
+    const random = crypto.randomBytes(4000).toString('base64');
+    await request.post('/session', {
+      baseUrl,
+      jar,
+      json: {
+        sub: '__test_sub__',
+        random,
+      },
+    });
+    assert.deepEqual(
+      jar.getCookies(baseUrl).map(({ key }) => key),
+      ['appSession.0', 'appSession.1']
+    );
+    const res = await request.get('/session', { baseUrl, json: true, jar });
+    assert.equal(res.statusCode, 200);
+    assert.deepEqual(res.body, {
+      sub: '__test_sub__',
+      random,
+    });
+  });
+
+  it('should handle unordered chunked cookies', async () => {
+    await setup();
+    const jar = request.jar();
+    const random = crypto.randomBytes(4000).toString('base64');
+    await request.post('/session', {
+      baseUrl,
+      jar,
+      json: {
+        sub: '__test_sub__',
+        random,
+      },
+    });
+    const newJar = request.jar();
+    jar
+      .getCookies(baseUrl)
+      .reverse()
+      .forEach(({ key, value }) =>
+        newJar.setCookie(`${key}=${value}`, baseUrl)
+      );
+    assert.deepEqual(
+      newJar.getCookies(baseUrl).map(({ key }) => key),
+      ['appSession.1', 'appSession.0']
+    );
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar: newJar,
+    });
+    assert.equal(res.statusCode, 200);
+    assert.deepEqual(res.body, {
+      sub: '__test_sub__',
+      random,
+    });
+  });
+
+  it('should not throw for malformed cookie chunks', async () => {
+    await setup();
+    const jar = request.jar();
+    jar.setCookie('appSession.0=foo', baseUrl);
+    jar.setCookie('appSession.1=bar', baseUrl);
+    const res = await request.get('/session', { baseUrl, json: true, jar });
+    assert.equal(res.statusCode, 200);
+  });
+
+  it('should set the default cookie options over http', async () => {
+    server = await createServer(
+      appSession(getConfig({ ...defaultConfig, baseURL: 'http://example.org' }))
+    );
+    const jar = request.jar();
+    await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `appSession=${encrypted}`,
+      },
+    });
+    const [cookie] = jar.getCookies(baseUrl);
+    assert.deepInclude(cookie, {
+      key: 'appSession',
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+      extensions: ['SameSite=Lax'],
+    });
+    const expDate = new Date(cookie.expires);
+    const now = Date.now();
+    assert.approximately(Math.floor((expDate - now) / 1000), 86400, 5);
+  });
+
+  it('should set the default cookie options over https', async () => {
+    server = await createServer(
+      appSession(
+        getConfig({ ...defaultConfig, baseURL: 'https://example.org' })
+      )
+    );
+    const jar = request.jar();
+    await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `appSession=${encrypted}`,
+      },
+    });
+    // Secure cookies not set over http
+    assert.isEmpty(jar.getCookies(baseUrl));
+  });
+
+  it('should set the custom cookie options', async () => {
+    server = await createServer(
+      appSession(
+        getConfig({
+          ...defaultConfig,
+          session: {
+            cookie: {
+              httpOnly: false,
+              sameSite: 'Strict',
+            },
+          },
+        })
+      )
+    );
+    const jar = request.jar();
+    await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `appSession=${encrypted}`,
+      },
+    });
+    const [cookie] = jar.getCookies(baseUrl);
+    assert.deepInclude(cookie, {
+      key: 'appSession',
+      httpOnly: false,
+      extensions: ['SameSite=Strict'],
+    });
+  });
+
+  it('should use a custom cookie name', async () => {
+    server = await createServer(
+      appSession(
+        getConfig({
+          ...defaultConfig,
+          session: { name: 'customName' },
+        })
+      )
+    );
+    const jar = request.jar();
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `customName=${encrypted}`,
+      },
+    });
+    const [cookie] = jar.getCookies(baseUrl);
+    assert.equal(res.statusCode, 200);
+    assert.equal(cookie.key, 'customName');
+  });
+
+  it('should set an ephemeral cookie', async () => {
+    server = await createServer(
+      appSession(
+        getConfig({
+          ...defaultConfig,
+          session: { cookie: { transient: true } },
+        })
+      )
+    );
+    const jar = request.jar();
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `appSession=${encrypted}`,
+      },
+    });
+    const [cookie] = jar.getCookies(baseUrl);
+    assert.equal(res.statusCode, 200);
+    assert.isFalse(cookie.hasOwnProperty('expires'));
+  });
+
+  it('should not throw for expired cookies', async () => {
+    const twoWeeks = 2 * 7 * 24 * 60 * 60 * 1000;
+    const clock = sinon.useFakeTimers({
+      now: Date.now(),
+      toFake: ['Date'],
+    });
+    await setup();
+    const jar = request.jar();
+    clock.tick(twoWeeks);
+    const res = await request.get('/session', {
+      baseUrl,
+      json: true,
+      jar,
+      headers: {
+        cookie: `appSession=${encrypted}`,
+      },
+    });
+    assert.equal(res.statusCode, 200);
+    clock.restore();
+  });
+
+  it('should throw for duplicate mw', async () => {
+    server = await createServer((req, res, next) => {
+      req.appSession = {};
+      appSession(getConfig(defaultConfig))(req, res, next);
+    });
+    const res = await request.get('/session', { baseUrl, json: true });
+    assert.equal(res.statusCode, 500);
+    assert.equal(
+      res.body.err.message,
+      'req[appSession] is already set, did you run this middleware twice?'
+    );
+  });
+
+  it('should throw for reassigning session', async () => {
+    server = await createServer((req, res, next) => {
+      appSession(getConfig(defaultConfig))(req, res, () => {
+        try {
+          req.appSession = {};
+          next();
+        } catch (e) {
+          next(e);
+        }
+      });
+    });
+    const res = await request.get('/session', { baseUrl, json: true });
+    assert.equal(res.statusCode, 500);
+    assert.equal(res.body.err.message, 'session object cannot be reassigned');
+  });
+
+  it('should not throw for reassigining session to empty', async () => {
+    server = await createServer((req, res, next) => {
+      appSession(getConfig(defaultConfig))(req, res, () => {
+        req.appSession = null;
+        req.appSession = undefined;
+        next();
+      });
+    });
+    const res = await request.get('/session', { baseUrl, json: true });
+    assert.equal(res.statusCode, 200);
+  });
+
+  it('should expire after 24hrs of inactivity by default', async () => {
+    const clock = sinon.useFakeTimers({ toFake: ['Date'] });
+    await setup();
+    const jar = await login({ sub: '__test_sub__' });
+    let res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isNotEmpty(res.body);
+    clock.tick(23 * HR_MS);
+    res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isNotEmpty(res.body);
+    clock.tick(25 * HR_MS);
+    res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isEmpty(res.body);
+    clock.restore();
+  });
+
+  it('should expire after 7days regardless of activity by default', async () => {
+    const clock = sinon.useFakeTimers({ toFake: ['Date'] });
+    await setup();
+    const jar = await login({ sub: '__test_sub__' });
+    let days = 7;
+    while (days--) {
+      clock.tick(23 * HR_MS);
+      let res = await request.get('/session', { baseUrl, jar, json: true });
+      assert.isNotEmpty(res.body);
+    }
+    clock.tick(8 * HR_MS);
+    let res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isEmpty(res.body);
+    clock.restore();
+  });
+
+  it('should expire only after defined absoluteDuration', async () => {
+    const clock = sinon.useFakeTimers({ toFake: ['Date'] });
+    server = await createServer(
+      appSession(
+        getConfig({
+          ...defaultConfig,
+          session: {
+            rolling: false,
+            absoluteDuration: 10 * 60 * 60,
+          },
+        })
+      )
+    );
+    const jar = await login({ sub: '__test_sub__' });
+    clock.tick(9 * HR_MS);
+    let res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isNotEmpty(res.body);
+    clock.tick(2 * HR_MS);
+    res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isEmpty(res.body);
+    clock.restore();
+  });
+
+  it('should expire only after defined rollingDuration period of inactivty', async () => {
+    const clock = sinon.useFakeTimers({ toFake: ['Date'] });
+    server = await createServer(
+      appSession(
+        getConfig({
+          ...defaultConfig,
+          session: {
+            rolling: true,
+            rollingDuration: 24 * 60 * 60,
+            absoluteDuration: false,
+          },
+        })
+      )
+    );
+    const jar = await login({ sub: '__test_sub__' });
+    let days = 30;
+    while (days--) {
+      clock.tick(23 * HR_MS);
+      let res = await request.get('/session', { baseUrl, jar, json: true });
+      assert.isNotEmpty(res.body);
+    }
+    clock.tick(25 * HR_MS);
+    let res = await request.get('/session', { baseUrl, jar, json: true });
+    assert.isEmpty(res.body);
+    clock.restore();
+  });
+});
