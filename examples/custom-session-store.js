6f7db7dc4de0c42997bc3361f510601263040a07
adamjmcgrath <adam.mcgrath@auth0.com>
2021-02-23 10:12:56 +0000

    Add memorystore example and `auth.Store` helper

--- examples/custom-session-store.js
+++ examples/custom-session-store.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const { auth } = require('../');
+const MemoryStore = require('memorystore')(auth);
 
 const app = express();
-
-class Store {
-  constructor() {
-    this.sessions = {};
-  }
-
-  get (id, cb) {
-    const val = this.sessions[id];
-    console.log('sessionStoreget', id, val);
-    process.nextTick(() => cb(null, val));
-  }
-  set (id, data, cb) {
-    console.log('sessionstoreSave', id, data);
-    this.sessions[id] = data;
-    process.nextTick(() => cb());
-  }
-  destroy (id, cb) {
-    console.log('sessionstoreDestroy', id);
-    delete this.sessions[id];
-    process.nextTick(() => cb());
-  }
-}
 
 app.use(
   auth({
     idpLogout: true,
-    sessionStore: new Store(),
+    sessionStore: new MemoryStore({
+      checkPeriod: 24 * 60 * 1000,
+    }),
   })
 );
 
