4fc24a893cf3a77cc50f724bfbf8a06ca09c896a
adamjmcgrath <adam.mcgrath@auth0.com>
2022-09-13 10:26:35 +0100

    test

--- examples/basic.js
+++ examples/basic.js
@@ -6,10 +6,21 @@ const app = express();
 app.use(
   auth({
     idpLogout: true,
+    authorizationParams: {
+      response_type: 'code',
+      scope: 'openid',
+    },
+    afterCallback: async (req, res, session, decodedState) => {
+      console.log('session: ', session); //here I can see the token in session
+      return {
+        ...session,
+      };
+    },
   })
 );
 
 app.get('/', (req, res) => {
+  console.log('at', req.oidc.accessToken);
   res.send(`hello ${req.oidc.user.sub}`);
 });
 
