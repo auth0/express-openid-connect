cf531efaf2407a01fa01da79c6473c1dc5a2e3ba
Ankush kumar <31571486+aks96@users.noreply.github.com>
2026-01-09 11:04:50 +0530

    fix: added condition for aysnc which uses callbacks (#742)

--- end-to-end/attempt-silent-login.test.js
+++ end-to-end/attempt-silent-login.test.js
@@ -73,26 +73,18 @@ describe('attempt silent login', async () => {
     assert.ok(loggedInCookies.find(({ name }) => name === 'appSession'));
 
     // Delete cookies using BrowserContext API
-    const cookiesToDelete = await context.cookies(baseUrl);
-    const appSessionCookie = cookiesToDelete.find(
-      ({ name }) => name === 'appSession',
-    );
-    const skipSilentLoginCookie = cookiesToDelete.find(
-      ({ name }) => name === 'skipSilentLogin',
+    const cookiesToDelete = loggedInCookies.filter(
+      (cookie) =>
+        cookie.name === 'appSession' || cookie.name === 'skipSilentLogin',
     );
 
-    if (appSessionCookie) {
-      await page.deleteCookie({
-        name: appSessionCookie.name,
-        domain: appSessionCookie.domain,
-        path: appSessionCookie.path,
-      });
-    }
-    if (skipSilentLoginCookie) {
-      await page.deleteCookie({
-        name: skipSilentLoginCookie.name,
-        domain: skipSilentLoginCookie.domain,
-        path: skipSilentLoginCookie.path,
+    for (const cookie of cookiesToDelete) {
+      await page.setCookie({
+        name: cookie.name,
+        value: '',
+        domain: cookie.domain,
+        path: cookie.path,
+        expires: 0, // This forces immediate expiration
       });
     }
 
